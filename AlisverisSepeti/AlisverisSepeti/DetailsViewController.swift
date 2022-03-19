a3ad011b9ec98e35e2b12309d7fe64c84da12799
hacisimsek <hacisimsek23@gmail.com>
2022-03-19 22:17:37 +0300

    some feature developmen

--- AlisverisSepeti/AlisverisSepeti/DetailsViewController.swift
+++ AlisverisSepeti/AlisverisSepeti/DetailsViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import CoreData
 
-class DetailsViewController: UIViewController {
+class DetailsViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var isimTextField: UITextField!
@@ -19,13 +20,54 @@ class DetailsViewController: UIViewController {
 
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(klavyeyiKapat))
         view.addGestureRecognizer(gestureRecognizer)
+        
+        imageView.isUserInteractionEnabled = true
+        let imageGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselSec))
+        imageView.addGestureRecognizer(imageGestureRecognizer)
+    }
+    @objc func gorselSec(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     @objc func klavyeyiKapat(){
         view.endEditing(true)
-            
     }
     
     @IBAction func submitButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let alisveris = NSEntityDescription.insertNewObject(forEntityName: "Alisveris", into: context)
+        alisveris.setValue(isimTextField.text!, forKey: "isim")
+        alisveris.setValue(bedenTextField.text!, forKey: "beden")
+        
+        if let fiyat = Int(fiyatTextField.text!){
+            alisveris.setValue(fiyat, forKey: "fiyat")
+        }
+        
+        //Universial Unique id
+        alisveris.setValue(UUID(), forKey: "id")
+        let data = imageView.image?.jpegData(compressionQuality: 0.5)
+        
+        alisveris.setValue(data, forKey: "gorsel")
+        
+        
+        do {
+            try context.save()
+            print("kayıt edildi")
+            
+        } catch  {
+            print("hata var")
+            fatalError("when image is saving this application down")
+        }
     }
     
 }
--- AlisverisSepeti/AlisverisSepeti/ViewController.swift
+++ AlisverisSepeti/AlisverisSepeti/ViewController.swift
@@ -6,20 +6,66 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var isimDizisi = [String]()
+    var idDizisi = [UUID]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(eklemeButonuEklendi))
+        
+        verileriAl()
+    }
+    
+    func verileriAl(){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Alisveris")
+        
+        do{
+            let sonuclar = try context.fetch(fetchRequest)
+            
+            for sonuc in sonuclar as! [NSManagedObject]{
+                if let isimler = sonuc.value(forKey: "isim") as? String {
+                    isimDizisi.append(isimler)
+                }
+                
+                if let id = sonuc.value(forKey: "id") as? UUID {
+                    idDizisi.append(id)
+                }
+                
+            }
+            tableView.reloadData()
+        }catch{
+            print("hata var")
+            
+        }
     }
 
     @objc func eklemeButonuEklendi(){
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return isimDizisi.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = isimDizisi[indexPath.row]
+        return cell
     }
     
 }
