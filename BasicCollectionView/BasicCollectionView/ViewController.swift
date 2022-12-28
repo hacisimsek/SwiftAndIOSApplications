05175366c739585009cdd4ce04a6ee21789823cd
Hacı Şimşek <73344347+hacisimsek@users.noreply.github.com>
2022-12-28 11:08:46 +0300

    bsaicCollectionView app is done

--- BasicCollectionView/BasicCollectionView/ViewController.swift
+++ BasicCollectionView/BasicCollectionView/ViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
+    let screen = UIScreen.main.bounds
     
     var ulkeler:[String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let tasarim:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//
+//        let genislik = self.collectionView.frame.size.width
+//
+//        tasarim.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+//
+//        tasarim.itemSize = CGSize(width: (genislik-30)/3, height: (genislik-30)/3)
+//
+//        tasarim.minimumInteritemSpacing = 5
+//
+//        tasarim.minimumLineSpacing = 5
         
         ulkeler = ["Türkiye","Almanya","Japonya","Rusya","İtalya","Fransa","Mısır"]
         
@@ -24,7 +37,7 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource {
+extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -34,11 +47,21 @@ extension ViewController:UICollectionViewDelegate,UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ulkeHucre", for: indexPath) as! CollectionViewHucre
         cell.hucreLable.text = ulkeler[indexPath.row]
+        cell.layer.borderColor = UIColor.blue.cgColor
+        cell.layer.borderWidth = 0.5
         
         return cell
         
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("Ulke Adı : \(ulkeler[indexPath.row])")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (self.screen.width / 4) + 22, height: (self.screen.width / 4) + 20 )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
     }
 }
